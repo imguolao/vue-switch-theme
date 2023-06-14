b0cf752eefafe06cda33bb1447aa4b807a30c53a
imguolao <guolao.mq@foxmail.com>
2023-06-14 23:47:03 +0800

    feat: injecting css into js

--- vite.build.config.ts
+++ vite.build.config.ts
@@ -2,10 +2,12 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
   plugins: [
     vue(),
+    cssInjectedByJsPlugin(),
     dts({
       tsConfigFilePath: resolve(__dirname, './tsconfig.build.json'),
     }),
